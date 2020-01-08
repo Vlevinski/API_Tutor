27467623026c57c06fb13edea146b49e05e5996b
Valerian Levinski <v.levinski@gmail.com>
2020-01-08 19:24:07 +0200

    init commit

--- flask_get_rates/main_pub.py
+++ flask_get_rates/main_pub.py
@@ -1,5 +1,5 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, jsonify
-from flask_googlecharts import GoogleCharts, BarChart, MaterialLineChart
+from flask import Flask, flash, redirect, render_template
+from flask_googlecharts import GoogleCharts,BubbleChart
 import json
 import urllib3
 
@@ -10,26 +10,37 @@ def getExchangeRates():
     rates = []
     ratesj = {}
     http = urllib3.PoolManager()
-    response = http.request('GET', 'http://data.fixer.io/api/latest?access_key=????')
+    response = http.request('GET', 'http://data.fixer.io/api/latest?access_key=??????????')
     rdata = response.data
     rdata = json.loads(rdata, parse_float=float)
     ratesj[ 'USD'] = rdata['rates']['USD']
     ratesj[ 'GBP'] = rdata['rates']['GBP']
     ratesj[ 'EUR'] = rdata['rates']['EUR']
     ratesj[ 'AUD'] = rdata['rates']['AUD']
+    ratesj[ 'CHF'] = rdata['rates']['CHF']
+    ratesj[ 'SGD'] = rdata['rates']['SGD']
+    ratesj[ 'CAD'] = rdata['rates']['CAD']
+    ratesj[ 'GEL'] = rdata['rates']['GEL']
+    ratesj[ 'BZD'] = rdata['rates']['BZD']
     return ratesj
 
 @app.route("/rates")
-def today_rates():
+def hotdogs():
     rates = getExchangeRates()
-    currency = BarChart("currency", options={"title": "Currency rates",
-                                                  "width": 500,
-                                                  "height": 300})
+    currency = BubbleChart("currency", options={"title": "Currency rates",
+        "hAxis": {"title": 'Currency'},
+        "vAxis": {"title": 'Rates'},
+                                                  "width": 1000,
+                                                  "height": 600})
     currency.add_column("string", "Currency")
     currency.add_column("number", "rates")
+    currency.add_column("number", "id")
+    currency.add_column("number", "color")
+    currency.add_column("number", "value")
+    id = 0.5
     for curr,vals in rates.items():
-        currency.add_rows([[curr, vals]])
-
+        currency.add_rows([[curr, id, vals, vals, vals]])
+        id += 1
     charts.register(currency)
     return render_template("rates.html", rates = rates)
 
@@ -39,3 +50,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+
